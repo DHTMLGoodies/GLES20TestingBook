2dd457761272fda46d70a416d515215ea66b7aeb
DHTMLGoodies <post@dhtmlgoodies.com>
2014-11-15 21:43:26 +0100

    First classes

---
+++ app/src/main/java/com/puzzleall/glesbook2/AirHockeyActivity.java
@@ -0,0 +1,54 @@
+package com.puzzleall.glesbook2;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class AirHockeyActivity extends ActionBarActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean renderedSet = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if(supportsEs2){
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+            // Assign our renderer
+            glSurfaceView.setRenderer(new AirHockeyRenderer());
+            renderedSet = true;
+        }else{
+            return;
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(renderedSet){
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(renderedSet){
+            glSurfaceView.onResume();
+        }
+    }
+}
---
+++ app/src/main/java/com/puzzleall/glesbook2/AirHockeyRenderer.java
@@ -0,0 +1,65 @@
+package com.puzzleall.glesbook2;
+
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+
+    // Two components(x,y) per vertex
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+
+    public AirHockeyRenderer() {
+        float[] tableVerticesWithTriangles = {
+                // Triangle 1
+                0f, 0f,
+                9f, 14f,
+                0f, 14f,
+                // Triangle 2
+                0f, 0f,
+                9f, 0f,
+                9f, 14f,
+                // Line 1
+                0f, 7f,
+                9f, 7f,
+                // Mallets
+                4.5f, 2f,
+                4.5f, 12f
+        };
+
+        // Allocate block of native memory, memory not managed by the garbage collector.
+        vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        // Copy vertex data into native memory
+        vertexData.put(tableVerticesWithTriangles);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        // Set the OpenGL Viewport to fill the entire surface
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
