d4b7b6ae95cc4aef7683dacb24c0bff221e3a175
DHTMLGoodies <post@dhtmlgoodies.com>
2014-11-19 10:29:04 +0100

    Object classes

--- app/src/main/java/com/puzzleall/glesbook2/AirHockeyRenderer.java
+++ app/src/main/java/com/puzzleall/glesbook2/AirHockeyRenderer.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 import com.puzzleall.glesbook2.objects.Mallet;
+import com.puzzleall.glesbook2.objects.Puck;
 import com.puzzleall.glesbook2.objects.Table;
 import com.puzzleall.glesbook2.programs.ColorShaderProgram;
 import com.puzzleall.glesbook2.programs.TextureShaderProgram;
@@ -17,14 +18,20 @@ import static android.opengl.GLES20.*;
 import static android.opengl.Matrix.multiplyMM;
 import static android.opengl.Matrix.rotateM;
 import static android.opengl.Matrix.setIdentityM;
+import static android.opengl.Matrix.setLookAtM;
 import static android.opengl.Matrix.translateM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private final Context context;
+    private final float[] viewMatrix = new float[16];
+    private final float[] viewProjectionMatrix = new float[16];
+    private final float[] modelViewProjectionMatrix = new float[16];
+
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
 
+    private Puck puck;
     private Table table;
     private Mallet mallet;
 
@@ -44,7 +51,9 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         table = new Table();
-        mallet = new Mallet();
+        mallet = new Mallet(0.08f, 0.15f, 32);
+
+        puck = new Puck(0.06f, 0.02f, 32);
 
         textureProgram = new TextureShaderProgram(context);
         colorProgram = new ColorShaderProgram(context);
@@ -57,44 +66,51 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         // Set the OpenGL Viewport to fill the entire surface
         glViewport(0, 0, width, height);
 
-        // Create projection matrix
-        MatrixHelper.perspectiveM(
-                projectionMatrix,
-                45, // Field of vision
-                (float) width / (float) height, // aspect
-                1f, // near plane
-                10f // far plane
-        );
-
-        // Set model matrix to identity matrix
-        setIdentityM(modelMatrix, 0);
-        // Move the matrix by 2 unites along the negative z-axis.
-        translateM(modelMatrix, 0, 0f, 0f, -2.5f);
-        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f);
-
-        final float[] temp = new float[16];
-        // Multiply projectionMatrix by modelMatrix and put the result inside temp
-        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
-        // Copy temp to projectionMatrix
-        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+        MatrixHelper.perspectiveM(projectionMatrix, 45, (float)width / (float) height, 1f, 10f);
+        setLookAtM(viewMatrix, 0, 0f, 1.2f, 2.2f, 0f,0f,0f, 0f, 1f, 0f);
     }
 
     @Override
     public void onDrawFrame(GL10 glUnused) {
         // Clear the rendering surface
         glClear(GL_COLOR_BUFFER_BIT);
+        // Multiply projectionMatrix and viewMatrix into viewProjectionMatrix
+        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
 
-        // Draw the table
+        positionTableInScene();
         textureProgram.useProgram();
-        textureProgram.setUniforms(projectionMatrix, texture);
+        textureProgram.setUniforms(modelViewProjectionMatrix, texture);
         table.bindData(textureProgram);
         table.draw();
 
         // Draw the mallets
+        positionObjectInScene(0f, mallet.height / 2, - 0.4f);
         colorProgram.useProgram();
-        colorProgram.setUniforms(projectionMatrix);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 0f,0f,1f);
         mallet.bindData(colorProgram);
         mallet.draw();
+        positionObjectInScene(0f, mallet.height / 2f, 0.4f);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 1f,0f,0f);
+        mallet.draw();
 
+        // Draw the puck
+        positionObjectInScene(0f, puck.height / 2f, 0f);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 0.8f, 0.8f, 1f);
+        puck.bindData(colorProgram);
+        puck.draw();
+
+    }
+
+    private void positionTableInScene(){
+        // Table table is defined in terms of X & Y coordinates, so e we rotate it 90 degrees to lie flat on the XZ plane
+        setIdentityM(modelMatrix, 0);
+        rotateM(modelMatrix, 0, -90f, 1f, 0f, 0f);
+        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
+    }
+
+    private void positionObjectInScene(float x, float y, float z){
+        setIdentityM(modelMatrix, 0);
+        translateM(modelMatrix, 0, x, y, z);
+        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
     }
 }
--- app/src/main/java/com/puzzleall/glesbook2/programs/ColorShaderProgram.java
+++ app/src/main/java/com/puzzleall/glesbook2/programs/ColorShaderProgram.java
@@ -12,20 +12,23 @@ public class ColorShaderProgram extends ShaderProgram {
     // Attribute locations
     private final int aPositionLocation;
     private final int aColorLocation;
+    private final int uColorLocation;
 
     public ColorShaderProgram(Context context){
         super(context, R.raw.simple_vertex_shader, R.raw.simple_fragment_shader);
 
-        // Retrieve uniform locations for the shader pgoram
+        // Retrieve uniform locations for the shader program
         uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
         // Retrieve attribute locations for the shader program
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
         aColorLocation = glGetAttribLocation(program, A_COLOR);
+        uColorLocation = glGetUniformLocation(program, U_COLOR);
     }
 
-    public void setUniforms(float[] matrix){
+    public void setUniforms(float[] matrix, float r, float g, float b){
         // Pass the matrix into the shader program
         glUniformMatrix4fv(uMatrixLocation, 1, false, matrix, 0);
+        glUniform4f(uColorLocation, r,g,b, 1f);
     }
 
     public int getPositionAttributeLocation(){
